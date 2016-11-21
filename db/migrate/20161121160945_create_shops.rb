0b9ec54173335c2887e7418d0af4fede078e0e49
Shota Miyazaki <jmsf0203@gmail.com>
2016-11-22 01:34:28 +0900

    shop modelを作成

--- db/migrate/20161121160945_create_shops.rb
+++ db/migrate/20161121160945_create_shops.rb
@@ -1,7 +1,10 @@
 class CreateShops < ActiveRecord::Migration
   def change
-    create_table :shops do |t|
-
+    create_table :shops, id: false, primary_key: :shop_id do |t|
+      t.column :shop_id, :integer, null: false
+      t.column :name, :string, null: false
+      t.column :longitude, :decimal, null: false
+      t.column :latitude, :decimal, null: false
       t.timestamps
     end
   end
