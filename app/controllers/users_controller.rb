35afff2bf0ee0f9541bb2a21e2a7dfefff0c0c9d
Shota Miyazaki <jmsf0203@gmail.com>
2017-01-18 21:52:53 +0900

    users_showを完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    id = params[:id]
+    @user = User.find(id)
+    row_sql = <<-SQL
+    select shops.shop_id, shops.name from users
+    inner join evaluations on users.id = evaluations.user_id
+    inner join photos on evaluations.photo_id = photos.id
+    inner join shops on photos.shop_id = shops.shop_id
+    where users.id = :user_id
+    SQL
+    args = [row_sql, user_id: @user.id]
+    sql = ActiveRecord::Base.send(:sanitize_sql_array, args)
+    @shops = ActiveRecord::Base.connection.select_all(sql)
+    p @shops[0]
+  end
 end
