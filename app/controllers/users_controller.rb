f4cb628eb106c191541575865f5a104d10c62508
Shota Miyazaki <jmsf0203@gmail.com>
2017-01-18 22:09:04 +0900

    Users#showをユニークな名前が表示されるように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,14 @@ class UsersController < ApplicationController
     id = params[:id]
     @user = User.find(id)
     row_sql = <<-SQL
-    select shops.shop_id, shops.name from users
-    inner join evaluations on users.id = evaluations.user_id
-    inner join photos on evaluations.photo_id = photos.id
-    inner join shops on photos.shop_id = shops.shop_id
-    where users.id = :user_id
+    select shops.name, shops.shop_id from shops
+      where shops.shop_id in
+      (
+        select photos.shop_id from users
+        inner join evaluations on users.id = evaluations.user_id
+        inner join photos on evaluations.photo_id = photos.id
+        where users.id = :user_id
+      )
     SQL
     args = [row_sql, user_id: @user.id]
     sql = ActiveRecord::Base.send(:sanitize_sql_array, args)
